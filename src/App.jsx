48de49afb40051eb095b53b43b423ba512e02922
David Murati <davidmurati1@gmail.com>
2024-09-02 22:30:15 -0400

    2 commit scraping

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import Scraper from './Scraper';
+import { BrowserRouter as Router, Route, Switch  } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Scraper />
+      <Router>
+     <Switch>
+        <Route exact path="/">
+          <Scraper />
+        </Route>
+     </Switch>
+     </Router>
     </div>
   );
 }
